7c2cba0fceee13d8555fc5042226a524326ddaf0
Victor Ayodeji <dejivictor9@gmail.com>
2022-10-14 12:57:20 -0700

    code updated

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ * main - entry point
+ *
+ * Return: success
+ */
+
+
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
