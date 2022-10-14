436ce227b19505e48de0b8557eff4c094164a4ef
Victor Ayodeji <dejivictor9@gmail.com>
2022-10-14 14:36:24 -0700

    did 4 no betty errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-
 /**
  * main - entry point
- * Return : 0 if successful
- * Desciption:Nice one
+ * Return: success
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
