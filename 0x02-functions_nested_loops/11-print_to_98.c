9df19a6c488077f58d6ef4102a25b789397298a4
Victor Ayodeji <dejivictor9@gmail.com>
2022-10-19 22:26:24 -0700

    last number

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,59 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: the beginning
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 99)
+	{
+		for (i = n; i < 99; i++)
+		{
+			print(i);
+		}
+	}
+	else
+	{
+		for (i = n; i > 97; i--)
+		{
+			print(i);
+		}
+	}
+	_putchar('\n');
+}
+/**
+ * print - prints a number
+ * @n: The number
+ */
+void print(int n)
+{
+	int i;
+
+	if (n < 0)
+	{
+		i = -n;
+		_putchar('-');
+	}
+	else
+	{
+		i = n;
+	}
+	if (i >= 100)
+	{
+		_putchar('0' + i / 100);
+		_putchar('0' + (i / 10) % 10);
+	}
+	else if (i >= 10)
+	{
+		_putchar('0' + i / 10);
+	}
+	_putchar('0' + i % 10);
+	if (n != 98)
+	{
+		_putchar(',');
+		_putchar(' ');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 void times_table(void);
 int _putchar(char c);
 int add(int, int);
+void print_to_98(int n);
