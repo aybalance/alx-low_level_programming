162b7e58a261d05d62c73b6c778a6c0e8c46f9dc
Victor Ayodeji <dejivictor9@gmail.com>
2022-10-19 21:45:14 -0700

    recovery

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * times_table - prints the time table
+ * Return: The time table
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+		if (i * j >= 10)
+		{
+			_putchar('0' + (i * j) / 10);
+			_putchar('0' + (i * j) % 10);
+
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		else
+		{
+			if (j != 0)
+			{
+				_putchar(' ');
+			}
+			_putchar('0' + (i * j) % 10);
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
+	}
+}
