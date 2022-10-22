5ce5ddcce82febcd979fcb273b6f27118e486761
Victor Ayodeji <dejivictor9@gmail.com>
2022-10-21 20:14:27 -0700

    how is it going

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - tests function that prints if a number is positive or negative
+ * Return: Success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
