fb8877025de97400933659002992b6aa758bab54
Victor Ayodeji <dejivictor9@gmail.com>
2022-10-21 21:39:59 -0700

    learnt something

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - entry point
+ * Return: Success
+ */
+
+int main(void)
+{
+	unsigned long i, prime, n = 612852475143;
+	for (i = 3; i <= n; i += 2)
+	{
+		while (n % i == 0)
+		{
+			prime = i;
+			n = n / i;
+		}
+	}
+	printf("%lu\n", prime);
+	return (0);
+}
